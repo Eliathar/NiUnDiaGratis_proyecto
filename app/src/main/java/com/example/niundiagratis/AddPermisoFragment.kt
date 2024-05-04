8e04187396cf113aecab8837a5e7055bffd86877
Eliathar <javpeba@yahoo.es>
2024-05-04 11:50:15 +0200

    NiUnDiaGratis v1.0 api34 android 14

--- app/src/main/java/com/example/niundiagratis/AddPermisoFragment.kt
+++ app/src/main/java/com/example/niundiagratis/AddPermisoFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.example.niundiagratis.DatabaseActive.databaseAct
 import com.example.niundiagratis.data.dao.DiasDisfrutadosDao
 import com.example.niundiagratis.data.dao.TiposDiasDao
 import com.example.niundiagratis.data.db.BBDDHandler
@@ -25,24 +26,23 @@ import kotlinx.coroutines.withContext
 import java.time.ZoneId
 import java.util.Date
 
-// TODO: Rename parameter arguments, choose names that match
 
+/* todo agregar comprobacion de dias festivos segun FuncionesExtra.comprobardiafestivo para no
+    contarlos como permiso */
 class AddPermisoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private lateinit var binding: FragmentAddPermisoBinding
     private lateinit var fechaInicio: Date
     private lateinit var fechaFinal: Date
-    private lateinit var nombreBD: String
-    private lateinit var entidad: DiasDisfrutados
     private val viewModelT: ViewModelSimple by lazy {
-        val database = NiUnDiaGratisBBDD.obtenerInstancia(requireContext(), nombreBD)
+        val database = NiUnDiaGratisBBDD.obtenerInstancia(requireContext())
         daoT = database.fTiposDiasDao()
         ViewModelSimple(daoT)
     }
     private lateinit var daoT: TiposDiasDao
     private lateinit var navController: NavController
-    private lateinit var database: NiUnDiaGratisBBDD
     private lateinit var dao: DiasDisfrutadosDao
+    private var difDias = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,19 +53,13 @@ class AddPermisoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         binding = FragmentAddPermisoBinding.inflate(inflater, container, false)
         val view = binding.root
-        //Obtenemos el nombre de la base de datos
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                nombreBD = BBDDHandler.crearBBDD(requireContext())
-            }
-        }
 
-        database = NiUnDiaGratisBBDD.obtenerInstancia(requireContext(), nombreBD)
-        daoT = database.fTiposDiasDao()
-        dao = database.fDiasDisfrutadosDao()
+        //database = NiUnDiaGratisBBDD.obtenerInstancia(requireContext(), dbSeleccionada)
+        daoT = databaseAct!!.fTiposDiasDao()
+        dao = databaseAct!!.fDiasDisfrutadosDao()
         navController = findNavController()
 
         return view
@@ -77,21 +71,21 @@ class AddPermisoFragment : Fragment() {
 
         spinConfig()
 //--------------------------------Botones-----------------------------------------------------------
-        binding.btnFechaIniPerm09.setOnClickListener(){
+        binding.btnFechaIniPerm09.setOnClickListener {
             showDatePickerDialog(requireContext()) { fechaSelec ->
                 fechaInicio = fechaSelec
-                binding.btnFechaIniPerm09.text = "Inicio: ${formatearFecha(fechaInicio)}"
+                binding.btnFechaIniPerm09.text = getString(R.string.inicio, formatearFecha(fechaInicio))
             }
 
         }
-        binding.btnFechaFinPerm09.setOnClickListener(){
+        binding.btnFechaFinPerm09.setOnClickListener {
             showDatePickerDialog(requireContext()) { fechaSelec ->
                 fechaFinal = fechaSelec
-                binding.btnFechaFinPerm09.text = "Inicio: ${formatearFecha(fechaFinal)}"
+                binding.btnFechaFinPerm09.text = getString(R.string.fin, formatearFecha(fechaFinal))
             }
 
         }
-        binding.buttonGuardar.setOnClickListener(){
+        binding.buttonGuardar09.setOnClickListener {
             btnCalcular()
         }
 
@@ -113,7 +107,6 @@ class AddPermisoFragment : Fragment() {
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
                 //No se ha realizado ninguna seleccion
             }
         }
@@ -127,10 +120,10 @@ class AddPermisoFragment : Fragment() {
                 viewModelT.obtenerTiposDiasList()
             }
             //Iteramos sobre la lista y obtenemos los nombres de los tipos de días
-            val nombresTiposActividades = tipoDiaDB?.map { it.nombreTipoDia } ?: emptyList()
+            val nombresTiposActividades = tipoDiaDB.map { it.nombreTipoDia }
 
             //Creamos un ArrayAdapter con la lista de nombres
-            val adapter = ArrayAdapter<String>(
+            val adapter = ArrayAdapter(
                 requireContext(),
                 android.R.layout.simple_spinner_item,
                 nombresTiposActividades
@@ -167,7 +160,6 @@ class AddPermisoFragment : Fragment() {
                     }
                 }
                 override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
                     //No se ha realizado ninguna seleccion
                 }
             }
@@ -179,42 +171,30 @@ class AddPermisoFragment : Fragment() {
 
         println("calcular pulsado")
         lifecycleScope.launch(Dispatchers.IO) {
-            println("A guardar datos1")
             //Obtenemos el tipo de dia disfrutado del spinner
             val tipoDiaDisfrutado = binding.spinnerTipo09.selectedItem.toString()
-            println("A guardar datos2 $tipoDiaDisfrutado")
-            val tipoDia = daoT.getTipoDiaById(tipoDiaDisfrutado)
+            daoT.getTipoDiaById(tipoDiaDisfrutado)
 
             //Creamos las variables para la resta de fechas, modificando el formato para obetener una medida de dias
             val fechaIni = fechaInicio.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
-            println("A guardar datos4")
             val fechaFin = fechaFinal.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
-            println("A guardar datos5")
+
             //Calculamos la diferencia en días
-            val difDias = java.time.temporal.ChronoUnit.DAYS.between(fechaIni, fechaFin).toInt()
-            println("A guardar datos6")
+            difDias = if(!binding.checkBox09.isChecked) {
+                comprobarDiaFestivo(fechaIni, fechaFin, databaseAct!!)
+            } else{
+                java.time.temporal.ChronoUnit.DAYS.between(fechaIni, fechaFin).toInt()+1
+            }
 
             //Asignamos los valores a una variable del tipo adecuado para guardar los datos
-            println("A guardar datos")
-            //Bucle para escribir un registro por dia disfrutado
-            var permisoNuevo: DiasDisfrutados? = null
-            val listaPermisos = mutableListOf<DiasDisfrutados>()
-            for (i in 0L..difDias) {
-            //Obtenemos el valor para el registro con cambio a formato Date pues esta en LocalDate
-            val fechaCon = Date.from(fechaIni.atStartOfDay(ZoneId.systemDefault()).toInstant())
-            println("A guardar datos7")
-                permisoNuevo = tipoDia.let { it1 ->
-                    DiasDisfrutados(
-                        id = 0,
-                        tipoDiaDis = tipoDiaDisfrutado,
-                        fechaCon = fechaCon
-                    )
-
-                }
-                listaPermisos.add(permisoNuevo)
+                val permisoNuevo = DiasDisfrutados(
+                    id = 0,
+                    tipoDiaDis = tipoDiaDisfrutado,
+                    fechaCon = fechaInicio,
+                    fechaFinPer = fechaFinal,
+                    diasTotales = difDias
+                )
             println(permisoNuevo)
-            println("A guardar datos8")
-            }
             /*
             -------------------------Creamos el cuadro de confirmacion------------------------------------------
             Estamos en un hilo secundario, pero el cuadro de dialogo solo se ejecuta en el hilo principal, no
@@ -224,34 +204,25 @@ class AddPermisoFragment : Fragment() {
             */
             withContext(Dispatchers.Main) {
                 val construct = AlertDialog.Builder(context)
-                println("A guardar datos9")
                 construct.setTitle("Confirmar datos")
-                println("A guardar datos10")
                 construct.setMessage(
                     "¿Estas seguro de que quieres guardar estos datos?:\n\n" +
                             "Tipo de dia: $tipoDiaDisfrutado\n" +
                             "Fecha de Inicio: $fechaIni\n" +
-                            "Fecha de Finalización: $fechaFin"
+                            "Fecha de Finalización: $fechaFin\n" +
+                            "Dias totales: $difDias"
                 )
-                println("A guardar datos11")
                 //Controlamos la reaccion de pulsar aceptar
 
-                construct.setPositiveButton("Aceptar") { dialog, wich ->
+                construct.setPositiveButton("Aceptar") { _, _ ->
                     runBlocking {
-                        if (permisoNuevo != null) {
-                            println("datos asignados")
-    //------------------------Volvemos a un hilo secundario para guardar los datos----------------------
-                            lifecycleScope.launch(Dispatchers.IO) {
-                                println("A guardar datos guardando")
-                                dao.insertAll(listaPermisos)
-                                BBDDHandler.actualizarComputoGlobal(database)
-
-                                println("A guardar datos terminado")
-
-                            }
-    //------------------------------------Fin hilo secundario-------------------------------------------
-                            println("datos guardados?")
+                        println("datos asignados")
+                        //------------------------Volvemos a un hilo secundario para guardar los datos----------------------
+                        lifecycleScope.launch(Dispatchers.IO) {
+                            dao.insert(permisoNuevo)
+                            BBDDHandler.actualizarComputoGlobal(databaseAct!!)
                         }
+                        //------------------------------------Fin hilo secundario-------------------------------------------
                         //------Cargamos el fragment home al guardar los datos en la base de datos----------
                         navController.navigate(R.id.nav_home)
                     }
